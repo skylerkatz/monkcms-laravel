e6cb485668d8fcef35560f9be27c0a86c7d2f7e3
Skyler Katz <skylerkatz@gmail.com>
2021-07-28 17:00:53 -0500

    convert to datasets

--- tests/Unit/QueryBuilderFindTest.php
+++ tests/Unit/QueryBuilderFindTest.php
@@ -3,57 +3,17 @@
 use Monkdev\MonkCms\Api\Find;
 use Monkdev\MonkCms\Api\QueryBuilder;
 
-it('can find by id', function () {
+it('finds by types with values', function ($type, $value, $result) {
     $queryBuilder = new QueryBuilder();
-    $queryBuilder->find(Find::BY_ID, 1);
+    $queryBuilder->find($type, $value);
 
-    expect($queryBuilder->buildQueryAsArray())->toMatchArray([
-        'find' => 1,
-    ]);
+    expect(invokeProtected($queryBuilder, 'buildQueryString'))->toContain($result);
+})->with([
+    [Find::BY_ID, '1', 'find_%3A_1'],
 
-    expect(invokeProtected($queryBuilder, 'buildQueryString'))->toContain('find_%3A_1');
-});
+    [Find::BY_SITE_SLUG, 'simple-site', 'find_site_%3A_simple-site'],
 
-it('can find by site slug', function () {
-    $queryBuilder = new QueryBuilder();
-    $queryBuilder->find(Find::BY_SITE_SLUG, 'simple-site');
-
-    expect($queryBuilder->buildQueryAsArray())->toMatchArray([
-        'find_site' => 'simple-site',
-    ]);
-
-    expect(invokeProtected($queryBuilder, 'buildQueryString'))->toContain('find_site_%3A_simple-site');
-});
-
-it('can find by a single category', function () {
-    $queryBuilder = new QueryBuilder();
-    $queryBuilder->find(Find::CATEGORY, 'love');
-
-    expect($queryBuilder->buildQueryAsArray())->toMatchArray([
-        'find_category' => 'love',
-    ]);
-
-    expect(invokeProtected($queryBuilder, 'buildQueryString'))->toContain('find_category_%3A_love');
-});
-
-it('can find by categories with categories as a string', function () {
-    $queryBuilder = new QueryBuilder();
-    $queryBuilder->find(Find::CATEGORIES, 'love,healing');
-
-    expect($queryBuilder->buildQueryAsArray())->toMatchArray([
-        'find_category' => 'love,healing',
-    ]);
-
-    expect(invokeProtected($queryBuilder, 'buildQueryString'))->toContain('find_category_%3A_love%2Chealing');
-});
-
-it('can find by categories with categories as an array', function () {
-    $queryBuilder = new QueryBuilder();
-    $queryBuilder->find(Find::CATEGORIES, ['love', 'healing']);
-
-    expect($queryBuilder->buildQueryAsArray())->toMatchArray([
-        'find_category' => 'love,healing',
-    ]);
-
-    expect(invokeProtected($queryBuilder, 'buildQueryString'))->toContain('find_category_%3A_love%2Chealing');
-});
+    [Find::CATEGORY, 'love', 'find_category_%3A_love'],
+    [Find::CATEGORIES, 'love,healing', 'find_category_%3A_love%2Chealing'],
+    [Find::CATEGORIES, ['love', 'healing'], 'find_category_%3A_love%2Chealing'],
+]);
