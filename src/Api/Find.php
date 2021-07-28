71034cea8ab651bfd3b8161308510e806643e22d
Skyler Katz <skylerkatz@gmail.com>
2021-07-28 16:29:52 -0500

    add find by site slug to query builder

--- src/Api/Find.php
+++ src/Api/Find.php
@@ -5,4 +5,5 @@ namespace Monkdev\MonkCms\Api;
 class Find
 {
     public const BY_ID = 'find';
+    public const BY_SITE_SLUG = 'find_site';
 }
--- tests/Unit/QueryBuilderFindTest.php
+++ tests/Unit/QueryBuilderFindTest.php
@@ -13,3 +13,14 @@ it('can find by id', function () {
 
     expect(invokeProtected($queryBuilder, 'buildQueryString'))->toContain('find_%3A_1');
 });
+
+it('can find by site slug', function () {
+    $queryBuilder = new QueryBuilder();
+    $queryBuilder->find(Find::BY_SITE_SLUG, 'simple-site');
+
+    expect($queryBuilder->buildQueryAsArray())->toMatchArray([
+        'find_site' => 'simple-site',
+    ]);
+
+    expect(invokeProtected($queryBuilder, 'buildQueryString'))->toContain('find_site_%3A_simple-site');
+});
