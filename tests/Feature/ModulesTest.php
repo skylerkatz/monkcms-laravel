583c7de16be352a1ba467dee8adf270116c5d143
Skyler Katz <skyler@monkdevelopment.com>
2021-09-05 10:00:59 -0500

    convert to higher order test
    Signed-off-by: Skyler Katz <skyler@monkdevelopment.com>

--- tests/Feature/ModulesTest.php
+++ tests/Feature/ModulesTest.php
@@ -3,23 +3,24 @@
 use Monkdev\MonkCms\Modules\Sermon;
 use Monkdev\MonkCms\Modules\Article;
 
-test('modules have a get method', function () {
-    expect(method_exists(Sermon::class, 'get'))->toBeTrue();
-});
+test('modules have a get method')
+    ->expect(method_exists(Sermon::class, 'get'))
+    ->toBeTrue();
 
-test('modules have debugQuery method', function () {
-    expect(method_exists(Sermon::class, 'debugQuery'))->toBeTrue();
-});
+test('modules have debugQuery method')
+    ->expect(method_exists(Sermon::class, 'debugQuery'))
+    ->toBeTrue();
 
-it('sets the module dynamically', function () {
-    expect((new Sermon())->debugQuery())->toBe(['module' => 'sermon', 'json' => true]);
-    expect((new Article())->debugQuery())->toBe(['module' => 'article', 'json' => true]);
-});
+it('sets the module dynamically')
+    ->expect((new Sermon())->debugQuery())
+    ->toBe(['module' => 'sermon', 'json' => true])
+    ->and((new Article())->debugQuery())
+    ->toBe(['module' => 'article', 'json' => true]);
 
-it('can set a display type', function () {
-    expect(Sermon::display('list')->debugQuery())->toBe([
+it('can set a display type')
+    ->expect(Sermon::display('list')->debugQuery())
+    ->toBe([
         'module' => 'sermon',
         'display' => 'list',
         'json' => true,
     ]);
-});
