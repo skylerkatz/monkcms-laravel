4d2c08e6958ce61e430b5710a569770605d282c2
Skyler Katz <skylerkatz@hey.com>
2021-10-31 16:57:35 -0500

    Add Supported Modules (#4)
    Signed-off-by: Skyler Katz <skyler@monkdevelopment.com>

---
+++ src/Modules/Member.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Monkdev\MonkCms\Modules;
+
+class Member extends Module
+{
+}
---
+++ src/Modules/Social/Instagram.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Monkdev\MonkCms\Modules\Social;
+
+use Monkdev\MonkCms\Modules\Module;
+
+class Instagram extends Module
+{
+}
